0a9d3c14eddedd1e05195d1e8208ecdc19cfae3f
Lily Beauvilliers <lily.beauvilliers@gmail.com>
2022-01-12 11:53:29 +0000

    rawre octane to page 126, updating song ratings

--- app/controllers/bands/band/songs.js
+++ app/controllers/bands/band/songs.js
@@ -1,8 +1,9 @@
 import Controller from '@ember/controller';
 import { tracked } from '@glimmer/tracking';
 import { action } from '@ember/object';
-import { Song } from 'rarwe/routes/bands';
+import Song from 'rarwe/models/song';
 import { inject as service } from '@ember/service';
+import fetch from 'fetch';
 
 export default class BandsBandSongsController extends Controller {
   @service catalog;
@@ -16,8 +17,37 @@ export default class BandsBandSongsController extends Controller {
   }
 
   @action
-  saveSong() {
-    let song = new Song({ title: this.title, band: this.model });
+  async saveSong() {
+    let payload = {
+      data: {
+        type: 'songs',
+        attributes: { title: this.title },
+        relationships: {
+          band: {
+            data: {
+              id: this.model.id,
+              type: 'bands',
+            },
+          },
+        },
+      },
+    };
+    let response = await fetch('/songs', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/vnd.api+json',
+      },
+      body: JSON.stringify(payload),
+    });
+    let json = await response.json();
+    let { id, attributes, relationships } = json.data;
+    let rels = {};
+
+    for (let relationshipName in relationships) {
+      rels[relationshipName] = relationships[relationshipName].links.related;
+    }
+
+    let song = new Song({ id, ...attributes }, rels);
     this.catalog.add('song', song);
     this.model.songs = [...this.model.songs, song];
     this.title = '';
